2c3d52f03af44d56775a24a0ea0103ce17e24157
Smudge <68658429+smuddgge@users.noreply.github.com>
2024-01-28 17:26:56 +0000

    Added essentials

---
+++ src/main/java/com/github/cozyplugins/cozydeliveries/CozyDeliveries.java
@@ -0,0 +1,17 @@
+package com.github.cozyplugins.cozydeliveries;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CozyDeliveries extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
